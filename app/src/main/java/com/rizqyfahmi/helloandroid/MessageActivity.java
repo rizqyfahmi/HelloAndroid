ceecec9ff0ed50d4fdaebf20917806c8ad2c68fd
rizqyfahmi <rizqyfahmi@yahoo.com>
2019-01-02 10:19:25 +0700

    1. Create new activity called "FinalActivity" (With the same way when we create "MessageActivity") 2. Add TextView into the app/res/layout/activity_final.xml 3. Add new value called "final_activity_message" in to the app/res/values/strings.xml 4. Set the value into the TextView we create in the second point 5. Add button into app/res/layout/activity_message.xml 6. Add new value called "final_activity_button" into the app/res/values/strings.xml 7. Set the value into the button we created in the 6th point 8. Set onClick Listener method called "openFinalActivity", then put some codes there 9. Go to app/manifests/AndroidManifest.xml, make some modifications there

--- app/src/main/java/com/rizqyfahmi/helloandroid/MessageActivity.java
+++ app/src/main/java/com/rizqyfahmi/helloandroid/MessageActivity.java
@@ -3,6 +3,7 @@ package com.rizqyfahmi.helloandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -17,5 +18,9 @@ public class MessageActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.display_message);
         textView.setText(message);
+    }
+
+    public void openFinalActivity(View view){
+        startActivity(new Intent(this, FinalActivity.class ));
     }
 }
